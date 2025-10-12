dbf402e6f690d62d16e887a3b0c9512684d98e2b
Ayush Yadav <official.ayush.066@gmail.com>
2025-10-12 22:46:30 +0530

    basic Expressions

---
+++ 07_Functions/loclals.tf
@@ -0,0 +1,22 @@
+
+locals {
+  Repo       = "my-${6 + 1}-repo"                                //Arithmetic operation : +, -, *, /, %
+  Comparison = 5 > 3                                             //Comparison operators : >, <, >=, <=, ==, !=
+  Logical    = (5 > 3) && (3 < 4)                                //Logical operators : &&, ||, !
+  Condition  = (5 > 3) ? "Five is greater" : "somethig is wrong" //Condition operator : condition ? true_value : false_value
+}
+
+output "operators" {
+  value = {
+    Repo       = local.Repo
+    Comparison = local.Comparison
+    Logical    = local.Logical
+    Condition  = local.Condition
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+ 
