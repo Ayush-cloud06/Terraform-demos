ed80bc7450213034346c9255d7b5cfa04485f065
Ayush Yadav <official.ayush.066@gmail.com>
2025-10-27 19:10:18 +0530

    multiple Instances

---
+++ 08_Multiple-Resources/Ec2_Resources/networking.tf
@@ -0,0 +1,23 @@
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = local.project
+  }
+}
+
+
+resource "aws_subnet" "main" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Name = "${local.project}-${count.index}"
+  }
+
+}
+
+
