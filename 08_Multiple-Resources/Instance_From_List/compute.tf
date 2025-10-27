6f082ca98df588bdea52eccf738a57b8cbb19280
Ayush Yadav <official.ayush.066@gmail.com>
2025-10-27 19:27:41 +0530

    Resources from List

---
+++ 08_Multiple-Resources/Instance_From_List/compute.tf
@@ -0,0 +1,39 @@
+locals {
+  ami_ids = {
+    ubuntu = data.aws_ami.ubuntu.id
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+
+
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
+  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+
+  tags = {
+    Name    = "${local.project}-${count.index}"
+    Project = local.project
+  }
+}
+
+
+
+
