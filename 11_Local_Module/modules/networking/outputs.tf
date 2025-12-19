4d3e4d5633e050eeb0536a950bb554685ae789ae
Ayush Yadav <official.ayush.066@gmail.com>
2025-12-19 23:23:04 +0530

    Module outputs

--- 11_Local_Module/modules/networking/outputs.tf
+++ 11_Local_Module/modules/networking/outputs.tf
@@ -1,4 +1,31 @@
 
+locals {
+  output_public_subnets = {
+    for key in keys(local.public_subnets) : key => {
+      subnet_id         = aws_subnet.this[key].id
+      availability_zone = aws_subnet.this[key].availability_zone
+    }
+  }
+
+  output_private_subnets = {
+    for key in keys(local.private_subnets) : key => {
+      subnet_id         = aws_subnet.this[key].id
+      availability_zone = aws_subnet.this[key].availability_zone
+    }
+  }
+}
+
+output "vpc_id" {
+  value       = aws_vpc.this.id
+  description = "The AWS ID from the created VPC"
+}
+
 output "public_subnet" {
-  value = local.public_subnets
+  value       = local.output_public_subnets
+  description = "Map of public subnets with their IDs and AZs"
+}
+
+output "private_subnet" {
+  value       = local.output_public_subnets
+  description = "Map of private subnets with their IDs and AZs"
 }
--- 11_Local_Module/outputs.tf
+++ 11_Local_Module/outputs.tf
@@ -1,3 +1,7 @@
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
 output "module_public_subnets" {
   value = module.networking.public_subnet
 }
