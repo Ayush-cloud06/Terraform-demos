4119d77d2b23bebd89c89d68ce04396b6e9a6bbe
Ayush Yadav <official.ayush.066@gmail.com>
2025-12-20 19:44:29 +0530

    precondition validation

---
+++ 12_Object_Validation/compute.tf
@@ -0,0 +1,40 @@
+
+locals {
+  allowed_instance_types = ["t2.micro", "t2.small", "t2.medium"]
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "this" {
+  ami                         = data.aws_ami.ubuntu.id
+  associate_public_ip_address = true
+  instance_type               = var.instance_type
+
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+
+  lifecycle {
+    precondition {
+      condition     = contains(local.allowed_instance_types, var.instance_type)
+      error_message = "The instance type"
+    }
+  }
+
+}
