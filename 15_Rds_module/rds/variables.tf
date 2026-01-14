0aa06b946c27c97789a6c85b1677d7f78d93feed
Ayush Yadav <official.ayush.066@gmail.com>
2026-01-14 19:10:19 +0530

    Variables

---
+++ 15_Rds_module/rds/variables.tf
@@ -0,0 +1,49 @@
+variable "project_name" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
+
+  validation {
+    condition     = contains(["db.t3.micro", var.instance_Class])
+    error_message = "only db.t4.micro is allowed"
+  }
+}
+
+variable "storage_size" {
+  type = number
+
+  validation {
+    condition     = var.storage_size > 5 && var.storage_size <= 10
+    error_message = "DB storage value must be between 5 and 10 GB"
+  }
+}
+
+variable "engine" {
+  type = string
+
+  validation {
+    condition     = contains(["postres-latest", "postgres-14"], var.engine)
+    error_message = "DB message must be postgres-14 or higher"
+  }
+}
+
+variable "credentials" {
+  type = object({
+    username = string
+    password = string
+  })
+
+  sensitive = true
+
+  validation {
+    condition = (
+      length(regexall("[a-zA-Z]+", var.credentaials.password)) > 0
+      && length(regexall("[0-9]+", var.credentaials.password)) > 0
+      && length(regexall("[a-zA-Z0-9]{6,}+", var.credentaials.password)) > 0
+
+    )
+    error_message = "Must contain 6 character, with 1 digit and 1 special character"
+  }
+}
