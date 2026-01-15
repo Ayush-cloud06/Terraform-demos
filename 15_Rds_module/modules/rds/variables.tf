fc04757747225bebe2bf058565f511258b8643a1
Ayush Yadav <official.ayush.066@gmail.com>
2026-01-15 23:57:13 +0530

    Variable & description

--- 15_Rds_module/modules/rds/variables.tf
+++ 15_Rds_module/modules/rds/variables.tf
@@ -1,10 +1,13 @@
+
 variable "project_name" {
-  type = string
+  type        = string
+  description = "The RDS instance name, and add relevant tags"
 }
 
 variable "instance_class" {
-  type    = string
-  default = "db.t3.micro"
+  type        = string
+  default     = "db.t3.micro"
+  description = "The instance class used to create the database under freetier account"
 
   validation {
     condition     = contains(["db.t3.micro"], var.instance_class)
@@ -13,8 +16,9 @@ variable "instance_class" {
 }
 
 variable "storage_size" {
-  type    = number
-  default = 10
+  type        = number
+  default     = 10
+  description = "The amout of data to allocate to database, should be between 5 and 10 GB"
 
   validation {
     condition     = var.storage_size > 5 && var.storage_size <= 10
@@ -23,8 +27,9 @@ variable "storage_size" {
 }
 
 variable "engine" {
-  type    = string
-  default = "postres-latest"
+  type        = string
+  default     = "postres-latest"
+  description = "Engine to use, only postgres in currently supported"
 
   validation {
     condition     = contains(["postres-latest", "postgres-14"], var.engine)
@@ -38,7 +43,8 @@ variable "credentials" {
     password = string
   })
 
-  sensitive = true
+  sensitive   = true
+  description = "The root username and password for the database"
 
   validation {
     condition = (
@@ -49,4 +55,14 @@ variable "credentials" {
     )
     error_message = "Must contain 6 character, with 1 digit and 1 special character"
   }
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subenet ids to deploy RDS instance in"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "Security group ids to attach to the RDS instance"
 }
